b5e5a78ed9d95c69a5a5a97632a5694e0281b75c
virtualzeth <virtualzeth@gmail.com>
2020-12-22 10:03:04 +0100

    Initial Recipe class

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,13 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import component.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Cook Book");
+        primaryStage.setScene(new Scene(root, 700, 675));
+        Recipe recipe = new Recipe("Te med ingefær, honning, citron og hvidløg", new String[]{"1 citron","3 dl vand",
+                "5 cm ingefær","1 fed presset hvidløg"}, "1. Kom en hel citron skåret i fire både i en gryde fyldt " +
+                "med 3 dl kogende vand. Tilsæt ca. 5 cm frisk ingefær. Lad blandingen koge op i 1 minuts tid. Tag " +
+                "det af varmen og lad det hvile i 10 min. 1 fed presset hvidløg blandes i sammen med 1 spsk. honning.",
+                "Du kan også variere teen og tilsætte en knivspids cayenne peber (for at løsne slim) eller komme andre " +
+                        "lindrende krydderier i som f.eks. kanel eller nelliker.");
         primaryStage.show();
     }
 
---
+++ src/component/Recipe.java
@@ -0,0 +1,54 @@
+package component;
+
+public class Recipe {
+    // EXAMPLE class variable
+    private String name;
+    private String[] ingredients;
+    private String instructions;
+    private String notes;
+
+    public Recipe(String name, String[] ingredients, String instructions, String notes) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.notes = notes;
+    }
+
+    public Recipe(String name, String[] ingredients, String instructions) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+    // EXAMPLE getter
+    public String getName() {
+        return name;
+    }
+    // EXAMPLE setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
